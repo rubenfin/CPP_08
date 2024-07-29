154f6da66fb79c14417c55972b6e060d2f49a8b2
rubenfin <rubenkfinnerud@gmail.com>
2024-07-29 11:28:40 +0200

    better tests for ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rfinneru <rfinneru@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/07/28 15:20:12 by rfinneru      #+#    #+#                 */
-/*   Updated: 2024/07/28 16:03:11 by rfinneru      ########   odam.nl         */
+/*   Updated: 2024/07/29 11:28:12 by rfinneru      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,15 @@ int main(void)
     auto stdarrIt = easyfind(stdarr, 1);
 
     if (stdarrIt != stdarr.end())
-        std::cout << "Found number at position: " << *stdarrIt << std::endl;
+        std::cout << "Found number at position in Array: " << *stdarrIt << std::endl;
     else 
-        std::cout << "Didn't finy number in arry" << std::endl;
+        std::cout << "Didn't find your number in the array" << std::endl;
+
+    auto vecIt = easyfind(vec, 30);
+
+    if (vecIt != vec.end())
+        std::cout << "Found number at position in vector: " << *vecIt << std::endl;
+    else 
+        std::cout << "Didn't find your number in the array" << std::endl;
     return (0);
 }
